a5ec9a875cddac460e357599274c484573202180
leesumi <leesumi1989@naver.com>
2017-10-14 16:32:08 +0900

    ts

---
+++ test.ts
@@ -0,0 +1,13 @@
+var str = "string1234";
+//var str2 = "test2";
+var num = 3;
+//var num2 = new Number(3);
+
+// console.log(typeof str);
+// console.log(num);
+// console.log(num2);
+// console.log(num == num2);
+// console.log(typeof num);
+// console.log(typeof num2);
+
+//c + s + b
